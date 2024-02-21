9b1b9318e4f5b09d29e6a6ed444f1cb969485aaf
RavirajMadan27032001 <160715358+RavirajMadan27032001@users.noreply.github.com>
2024-02-21 13:36:59 +0530

    Create CharAlphabet.java

---
+++ CharAlphabet.java
@@ -0,0 +1,12 @@
+public class CharAlphabet
+{
+public static void main(String[] args)
+{
+char ch='@';
+String result=((ch>=65&&ch<=90)||(ch>=97&&ch<=122))?("The"+ch+" Character is a Alphabet."):("The"+ch+"Character is not a Alphabet.");
+
+System.out.println(result); 
+
+
+}
+}
